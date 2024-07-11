666d513ba46e78d0676d0c065aca2039b020b7cd
npearl <67296858+n-pearl@users.noreply.github.com>
2024-07-10 17:39:45 -0700

    Logging update + favicon

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for, flash
+import logging
 import pulls
 import plotly
 import plotly.express as px
@@ -7,27 +8,43 @@ import os
 
 app = Flask(__name__)
 
+logging.basicConfig(
+    filename='app.log',
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+
+logger = logging.getLogger(__name__)
+
 @app.route('/')
 def index():
+    logger.info('Rendering index page')
     return render_template('index.html')
 
 @app.route('/results', methods=['POST'])
 def results():
-    api_key = request.form['api_key']
-    start_date = request.form['start_date']
-    end_date = request.form['end_date']
-    news_source = request.form.get('news_source', '')
-    subject = request.form.get('subject', '')
-    
-    articles = pulls.fetch_news(api_key, start_date, end_date, news_source, subject)
-    sentiment_df = pulls.analyze_sentiments(articles)
-    
-    fig_polarity, fig_subjectivity = pulls.generate_graphs(sentiment_df)
-    
-    graph_polarity = json.dumps(fig_polarity, cls=plotly.utils.PlotlyJSONEncoder)
-    graph_subjectivity = json.dumps(fig_subjectivity, cls=plotly.utils.PlotlyJSONEncoder)
-    
-    return render_template('results.html', tables=[sentiment_df.to_html(classes='data')], titles=sentiment_df.columns.values, graph_polarity=graph_polarity, graph_subjectivity=graph_subjectivity)
+    try:
+        api_key = request.form['api_key']
+        start_date = request.form['start_date']
+        end_date = request.form['end_date']
+        news_source = request.form.get('news_source', '')
+        subject = request.form.get('subject', '')
+
+        logger.info(f"Received request with API key: {api_key}, start date: {start_date}, end date: {end_date}, news source: {news_source}, subject: {subject}")
+        
+        articles = pulls.fetch_news(api_key, start_date, end_date, news_source, subject)
+        sentiment_df = pulls.analyze_sentiments(articles)
+        
+        fig_polarity, fig_subjectivity = pulls.generate_graphs(sentiment_df)
+        
+        graph_polarity = json.dumps(fig_polarity, cls=plotly.utils.PlotlyJSONEncoder)
+        graph_subjectivity = json.dumps(fig_subjectivity, cls=plotly.utils.PlotlyJSONEncoder)
+        
+        return render_template('results.html', tables=[sentiment_df.to_html(classes='data')], titles=sentiment_df.columns.values, graph_polarity=graph_polarity, graph_subjectivity=graph_subjectivity)
+    except Exception as e:
+        logger.error(f"Error processing request: {e}")
+        flash(str(e))
+        return redirect(url_for('index'))
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port= int(os.environ.get('PORT', 5000)))
--- pulls.py
+++ pulls.py
@@ -1,50 +1,55 @@
 import requests
+import logging
 import pandas as pd
 from datetime import datetime, timedelta
 from textblob import TextBlob
 import plotly.express as px
 
-# Function to fetch news articles
-def fetch_news(api_key, start_date, end_date, news_source = None, subject = None):
-    url = "https://newsapi.org/v2/everything"
-    query = subject if subject else ""
-    params = {
-        'q': query,
-        'from': start_date,
-        'to': end_date,
-        'sortBy': 'publishedAt',
-        'apiKey': api_key,
-        'language': 'en',
-    }
-    if news_source:
-        params['sources'] = news_source
+logger = logging.getLogger(__name__)
 
-    response = requests.get(url, params=params)
-    articles = response.json().get('articles', [])
-    
-    return articles
 
-# Function to analyze sentiments
+def fetch_news(api_key, start_date, end_date, news_source=None, subject=None):
+    logger.info(f"Fetching news data from {start_date} to {end_date} for source: {news_source} and subject: {subject}")
+    try:
+        url = f"https://newsapi.org/v2/everything?q={subject}&from={start_date}&to={end_date}&apiKey={api_key}"
+        if news_source:
+            url += f"&sources={news_source}"
+        
+        response = requests.get(url)
+        response.raise_for_status()
+        articles = response.json()
+        logger.info("Articles fetched successfully")
+        return articles
+    except requests.exceptions.RequestException as e:
+        logger.error(f"Error fetching data: {e}")
+        raise
+
+
 def analyze_sentiments(articles):
-    data = []
-    for article in articles:
-        published_at = article.get('publishedAt')
-        content = article.get('content')
-        title = article.get('title', 'No Title')
-        link = article.get('url')
-        description = article.get('description')
-        if content and published_at:
-            analysis = TextBlob(content)
-            polarity = analysis.sentiment.polarity #type: ignore
-            subjectivity = analysis.sentiment.subjectivity #type: ignore
-            data.append({
-                'title': title,
-                'published_at': published_at,
-                'link': link,
-                'description': description,
-                'polarity': polarity,
-                'subjectivity': subjectivity
-            })
+    try:
+        data = []
+        for article in articles:
+            published_at = article.get('publishedAt')
+            content = article.get('content')
+            title = article.get('title', 'No Title')
+            link = article.get('url')
+            description = article.get('description')
+            if content and published_at:
+                analysis = TextBlob(content)
+                polarity = analysis.sentiment.polarity #type: ignore
+                subjectivity = analysis.sentiment.subjectivity #type: ignore
+                data.append({
+                    'title': title,
+                    'published_at': published_at,
+                    'link': link,
+                    'description': description,
+                    'polarity': polarity,
+                    'subjectivity': subjectivity
+                })
+                logger.info('Sentiment analysis performed successfully')
+    except Exception as e:
+        logger.error(f"Error during sentiment analysis: {e}")
+        raise
     
     if data:
         df = pd.DataFrame(data)
@@ -52,15 +57,15 @@ def analyze_sentiments(articles):
         return df
     else:
         print("No valid articles found")
-        return pd.DataFrame()  # Return an empty DataFrame if no valid articles are found
+        return pd.DataFrame()
     
-# Function to generate and return graphs
+
 def generate_graphs(df):
     fig_polarity = px.line(df, x='published_at', y='polarity', title='Polarity Over Time')
     fig_subjectivity = px.line(df, x='published_at', y='subjectivity', title='Subjectivity Over Time')
     return fig_polarity, fig_subjectivity
 
-# Function to print articles in a readable format
+
 def print_articles(df):
     if df.empty:
         print("No data to display")
@@ -72,15 +77,3 @@ def print_articles(df):
             print(f"Polarity: {article['polarity']}, Subjectivity: {article['subjectivity']}")
             print(f"Description: {article['description']}")
             print(f"Published At: {article['published_at']}")
-
-# Example usage
-#if __name__ == "__main__":
-#    START_DATE = (datetime.now() - timedelta(days=30)).strftime('%Y-%m-%d')
-#    END_DATE = datetime.now().strftime('%Y-%m-%d')
-#    NEWS_SOURCE = 'cnn'  # Example: 'cnn', 'bbc-news', etc.
-#    SUBJECT = 'tesla'
-#    key = ''
-#    
-#    articles = fetch_news(START_DATE, END_DATE, key, NEWS_SOURCE, SUBJECT)
-#    sentiment_df = analyze_sentiments(articles)
-#    print_articles(sentiment_df)
